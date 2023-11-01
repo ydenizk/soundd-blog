c81bbd6757f37521fe4ccb6395fb57c551562197
Denız  Kurt <denizkurt@192.168.1.62>
2023-11-01 22:50:54 +0300

    en url oplmadı ,eskıye donus

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import SideCard from "./components/sideCard/sideCard";
 const getData = async () => {
   
   try {
-    const res = await fetch(`${process.env.POST_URL}`, {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/posts`, {
       cache: "no-store",
     });
     if (res.ok) {
